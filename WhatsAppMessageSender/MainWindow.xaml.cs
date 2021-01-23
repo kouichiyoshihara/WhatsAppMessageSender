5d884a6e00fe978e779e2b7e75801c743f5ef793
Qdot605 <connoisseurbusiness@gmail.com>
2021-01-23 18:00:38 +0900

    ASCII修正

--- WhatsAppMessageSender/MainWindow.xaml.cs
+++ WhatsAppMessageSender/MainWindow.xaml.cs
@@ -2,8 +2,8 @@
 using WhatsAppMessage.Data;
 using MahApps.Metro.Controls;
 using System.Data;
+using System.Drawing;
 using MessageBox = System.Windows.MessageBox;
-using Microsoft.Win32;
 using System.Windows.Controls;
 using static WhatsAppMessage.Data.WhatsAppViewModel;
 using System.Collections.Generic;
@@ -19,6 +19,12 @@ using System.Threading.Tasks;
 using Windows.UI;
 using System.Windows.Media;
 using System.Windows.Controls.Primitives;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using System.Management;
+using System.Drawing.Imaging;
+using OpenCvSharp;
 
 namespace WhatsAppMessage
 {
@@ -51,43 +57,89 @@ namespace WhatsAppMessage
         /// <summary> send message button </summary>
         private void send_button_Click(object sender, RoutedEventArgs e)
         {
-
-            //テスト用の電卓計算
-            //startCalc();
-
-            /*
-            MessageBoxResult result = MessageBox.Show("Are you sure to send ?",
-                "Confirmation",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Warning);
-
-
-            if (result == MessageBoxResult.OK)
-            {
-                List<string> sendList = new List<string>();
-                // OK case
-                ObservableCollection<PhoneNumViewModel> phoneNumList = (ObservableCollection<PhoneNumViewModel>)this.dataGrid.ItemsSource;
-                foreach (PhoneNumViewModel phoneNum in phoneNumList)
+            //using (Mat mat = new Mat(@"C:\Users\ko198\source\repos\WhatsAppMessageSender\WhatsAppMessageSender\bin\Debug\wkConfig\shot_raw.png"))
+                // WhatsAppデスクトップアプリ（プロセス名：WhatsApp）をアクティブに
+                /*
+                Process[] processt = Process.GetProcessesByName("WhatsApp");
+                var processWA_t = new Process();
+                foreach (var p in processt)
                 {
-                    if (phoneNum.IsChecked)
+                   if (p.MainWindowTitle.Equals("WhatsApp"))
                     {
-                        sendList.Add(phoneNum.PhoneNumber);
+                        processWA_t = p;
+                        break;
                     }
                 }
-                var message = this.WhatsAppMessage.Text;
-                WhatsAppMessageSender wams = new WhatsAppMessageSender();
-                var process = Process.Start("C:\\Users\\ko198\\AppData\\Local\\WhatsApp\\WhatsApp.exe");
 
-                foreach (string sendTo in sendList)
+                List<Process> tent = new List<Process>();
+                var sss = GetChildProcesses(processWA_t);
+                foreach (var aa in sss) 
                 {
-                    wams.sendMessage(sendTo, message);
+                    tent.Add(aa);
+                    var a1 = GetChildProcesses(aa);
+                    foreach (var b in a1)
+                    {
+                        tent.Add(b);
+                        var b1 = GetChildProcesses(b);
+                        foreach (var b2 in b1)
+                        {
+                            tent.Add(b2);
+                        }
+                    }
                 }
-                MessageBox.Show("Complete");
-            }
-            */
+                var a = 1;
+                */
+                /*
+                Process[] process = Process.GetProcessesByName("WhatsApp");
+                if (process.Length > 0)
+                {
+                    foreach (var p in process)
+                    {
+                        if (p.MainWindowTitle.Equals("WhatsApp"))
+                        {
+                            Interaction.AppActivate(p.Id);
+                            break;
+                        }
+                    }
+                }
+                SendKeys.SendWait("{ENTER}");
+                */
+                //テスト用の電卓計算
+                //startCalc();
 
-            // チェックボックスが押下されていることを確認
-            if (!((bool)this.chk1.IsChecked && (bool)this.chk2.IsChecked))
+                /*
+                MessageBoxResult result = MessageBox.Show("Are you sure to send ?",
+                    "Confirmation",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning);
+
+
+                if (result == MessageBoxResult.OK)
+                {
+                    List<string> sendList = new List<string>();
+                    // OK case
+                    ObservableCollection<PhoneNumViewModel> phoneNumList = (ObservableCollection<PhoneNumViewModel>)this.dataGrid.ItemsSource;
+                    foreach (PhoneNumViewModel phoneNum in phoneNumList)
+                    {
+                        if (phoneNum.IsChecked)
+                        {
+                            sendList.Add(phoneNum.PhoneNumber);
+                        }
+                    }
+                    var message = this.WhatsAppMessage.Text;
+                    WhatsAppMessageSender wams = new WhatsAppMessageSender();
+                    var process = Process.Start("C:\\Users\\ko198\\AppData\\Local\\WhatsApp\\WhatsApp.exe");
+
+                    foreach (string sendTo in sendList)
+                    {
+                        wams.sendMessage(sendTo, message);
+                    }
+                    MessageBox.Show("Complete");
+                }
+                */
+
+                // チェックボックスが押下されていることを確認
+                if (!((bool)this.chk1.IsChecked && (bool)this.chk2.IsChecked))
             {
                 MessageBox.Show("Please check confirmation before sending");
                 return;
@@ -104,21 +156,64 @@ namespace WhatsAppMessage
             {
                 // メッセージボックスの中身を取得
                 string message = this.WhatsAppMessage.Text;
+                message = message.Replace("%", "%25");          // %変換
+
+                message = message.Replace("%0d", "%250d");      // \rのインジェクションを防ぐ
+                message = message.Replace("%0a", "%250a");      // \nのインジェクションを防ぐ
+                message = message.Replace("%20", "%2520");      // 半角スペースのインジェクションを防ぐ
+                message = message.Replace("%22", "%2522");      // "のインジェクションを防ぐ
+                message = message.Replace("%23", "%2523");      // #のインジェクションを防ぐ
+                message = message.Replace("%24", "%2524");      // $のインジェクションを防ぐ
+                message = message.Replace("%26", "%2526");      // &のインジェクションを防ぐ
+                message = message.Replace("%2C", "%252C");      // ,のインジェクションを防ぐ
+                //message = message.Replace("|", "");           // |のインジェクションを防ぐ）
+                message = message.Replace("%40", "%2540");      // @のインジェクションを防ぐ
+                message = message.Replace("%2B", "%252B");      // +のインジェクションを防ぐ
+                message = message.Replace("%3A", "%253A");      // :のインジェクションを防ぐ
+                message = message.Replace("%3B", "%253B");      // ;のインジェクションを防ぐ
+                message = message.Replace("%3D", "%253D");      // =のインジェクションを防ぐ
+                message = message.Replace("%3F", "%253F");      // ?のインジェクションを防ぐ
+                message = message.Replace("%2F", "%252F");      // /のインジェクションを防ぐ
+                // 特殊文字変換
                 message = message.Replace("\r\n", "%0d%0a");    // 改行変換
                 message = message.Replace(" ", "%20");          // 半角スペース変換
-                message = message.Replace("　", "%E3%80%80");   // 全角スペース変換
+                message = message.Replace("\"", "%22");         // "変換
+                message = message.Replace("#", "%23");          // #変換
+                message = message.Replace("$", "%24");          // $変換
+                message = message.Replace("&", "%26");          // &変換
+                message = message.Replace(",", "%2C");          // ,変換
+                //message = message.Replace("|", "");             // |変換（使用不可）
+                message = message.Replace("@", "%40");          // @変換
+                message = message.Replace("+", "%2B");          // +変換
+                message = message.Replace(":", "%3A");          // :変換
+                message = message.Replace(";", "%3B");          // ;変換
+                message = message.Replace("=", "%3D");          // =変換
+                message = message.Replace("?", "%3F");          // ?変換
+                message = message.Replace("/", "%2F");          // /変換
+
+
+
+
 
                 // チェックがついている宛先を宛先リストとして保存
                 List<string> sendList = new List<string>();
                 ObservableCollection<PhoneNumViewModel> customerList = (ObservableCollection<PhoneNumViewModel>)this.dataGrid.ItemsSource;
 
-                // WhatsAppプロセスを起動
-                var processWA = new Process();
-
+                
+                // インデックス
                 int index = 0;
+                
+                // WhatsAppアプリに移行できたかどうか
+                bool errFlg = false;
+
                 // 一斉送信開始
                 foreach (PhoneNumViewModel record in customerList)
                 {
+                    errFlg = false;
+
+                    // WhatsAppプロセスを起動
+                    var processWA = new Process();
+
                     // チェックがついている宛先のみ処理
                     if (record.IsChecked)
                     {
@@ -126,109 +221,85 @@ namespace WhatsAppMessage
 
                         // Webブラウザ起動
                         var processWB = Process.Start(@"chrome.exe", url);
-                        Thread.Sleep(4000);
-                        //processWB.WaitForExit(8000);
+                        //Thread.Sleep(4000);
+                        processWB.WaitForExit(10000);
                         processWB.CloseMainWindow(); // Webブラウザを閉じる
-
-                        // WhatsAppアプリに移行できたかどうか
-                        bool findFlg = false;
                         
                         try
                         {
                             Thread.Sleep(4000);
 
-                            // UWPだといろいろあるようなので、ProcessをタイトルからWhatsAppを探す
-                            foreach (var p in Process.GetProcesses())
+                            // WhatsAppデスクトップアプリ（プロセス名：WhatsApp）をアクティブに
+                            Process[] process = Process.GetProcessesByName("WhatsApp");
+                            
+                            if (process.Length > 0)
                             {
-                                if (p.MainWindowTitle.Equals("WhatsApp"))
+                                foreach (var p in process)
                                 {
-
-                                    processWA = p;
-                                    findFlg = true;
-                                    break;
-                                }
-                            }
-
-                            // WhatsAppが見つからなかったときはNGにする
-                            if (!findFlg)
-                            {
-                                customerList[index].Judge = "NG";
-                                this.dataGrid.ItemsSource = customerList;
-                                this.dataGrid.Items.Refresh();
-                            }
-
-                            // エラーが起きた時の処理を実装 
-                            try
-                            {
-                                var mainForm = AutomationElement.FromHandle(processWA.MainWindowHandle);
-
-                                Thread.Sleep(3000); // OKのエレメントを取得するまで待ち時間を設定
-                                //var btnOK = mainForm.FindFirst(TreeScope.Descendants,
-                                //   new PropertyCondition(AutomationElement.NameProperty, "OK"));
-
-                                // タスク設定し、４秒間応答がなければ正常終了していることとする
-                                try
-                                {
-                                    /*
-                                    var btnOK = Task.Run(() => FindButtonsByName(mainForm, "OK").First()
-                                    .GetCurrentPattern(InvokePattern.Pattern) as InvokePattern);
-                                    if (btnOK.Wait(TimeSpan.FromSeconds(4)))
+                                    if (p.MainWindowTitle.Equals("WhatsApp"))
                                     {
-                                        customerList[index].Judge = "NG";
-                                        this.dataGrid.ItemsSource = customerList;
-                                        this.dataGrid.Items.Refresh();
-                                    }
-                                    */
-                                    var btnOK = Task.Run(() => mainForm.FindFirst(TreeScope.Descendants,
-                                       new PropertyCondition(AutomationElement.NameProperty, "OK")));
-                                    if (btnOK.Wait(TimeSpan.FromSeconds(4)))
-                                    {
-                                        customerList[index].Judge = "NG";
-                                        this.dataGrid.ItemsSource = customerList;
-                                        this.dataGrid.Items.Refresh();
-                                        processWA.CloseMainWindow();
+                                        Interaction.AppActivate(p.Id);
+                                        processWA = p;
+                                        break;
                                     }
                                 }
-                                catch
-                                {
-                                    // 処理継続
-                                    System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                                }
-                                finally
-                                {
-                                    System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                                }
                             }
-                            catch(Exception ex)
+
+                            // アクティブウィンドウのWhatsApp画面を保存する
+                            string stCurrentDir = System.IO.Directory.GetCurrentDirectory();
+                            string waImg = stCurrentDir + "\\wkConfig\\shot_raw.png";
+
+                            RECT r;
+                            IntPtr active = GetForegroundWindow();
+                            GetWindowRect(active, out r);
+                            Rectangle rect = new Rectangle(r.left, r.top, r.right - r.left, r.bottom - r.top);
+
+                            Bitmap bmp = new Bitmap(rect.Width, rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                            using (Graphics g = Graphics.FromImage(bmp))
                             {
-                                // 処理継続
+                                g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
                             }
-                            finally
-                            {
-                                // 送信するためのエンターキーを押下
-                                Thread.Sleep(1000);
-                                System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                            }
+
+                            bmp.Save(@waImg, ImageFormat.Png);
+
+                            // 送信可能か判定
+                            string templateImg = stCurrentDir + "\\wkConfig\\template\\template.png";
+                            string templateImg2 = stCurrentDir + "\\wkConfig\\template\\template_2.png";
+                            errFlg = (!judgeMatch(waImg, templateImg) && !judgeMatch(waImg, templateImg2));
+
                         }
                         finally
                         {
+                            // errorが見つからなかったときはNGにする
+                            if (errFlg)
+                            {
+                                customerList[index].Judge = "NG";
+                                this.dataGrid.ItemsSource = customerList;
+                            }
+                            this.dataGrid.Items.Refresh();
+                            //Thread.Sleep(1000);
+                            //System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                            Interaction.AppActivate(processWA.Id);
+                            SendKeys.SendWait("{ENTER}");
                             Thread.Sleep(1000);
-                            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                            
+                            processWA.CloseMainWindow();
+
                         }
                     }
                     index++;
+                    //processWA.CloseMainWindow();
                 }
-                try
-                {
-                    processWA.CloseMainWindow();
-                }
-                catch
-                {
-                    // 処理継続
-                }
-                            
+
                 // 処理終了メッセージ表示
+                //if (Environment.Is64BitProcess)
+                //{
+                //    MessageBox.Show("64bit");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("32bit");
+                //}
                 MessageBox.Show("Complete");
             }
 
@@ -274,6 +345,41 @@ namespace WhatsAppMessage
 
         }
 
+        // 画像マッチング
+        private bool judgeMatch(string rawImgPath, string tempImgPath)
+        {
+            // 検索対象の画像
+            using (Mat mat = new Mat(@rawImgPath))
+            // テンプレート画像
+            using (Mat temp = new Mat(@tempImgPath))
+            
+            using (Mat result = new Mat())
+            {
+
+                // テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                // 類似度が最大/最小となる画素の位置を調べる
+                OpenCvSharp.Point minloc, maxloc;
+                double minval, maxval;
+                Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                // しきい値で判断
+                var threshold = 0.90;
+                if (maxval >= threshold)
+                {
+                    return true;
+                }
+                else
+                {
+                    // 見つからない
+                    return false;
+                }
+            }
+        }
+
+
+
         public static DataTable DataViewAsDataTable(DataView dv)
         {
             DataTable dt = dv.Table.Clone();
@@ -281,8 +387,6 @@ namespace WhatsAppMessage
                 dt.ImportRow(drv.Row);
             return dt;
         }
-
-
 
         private static void startCalc()
         {
@@ -340,7 +444,34 @@ namespace WhatsAppMessage
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
 
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
 
-        
+        [DllImport("User32.Dll")]
+        static extern int GetWindowRect(IntPtr hWnd, out RECT rect);
+
+        [DllImport("user32.dll")]
+        extern static IntPtr GetForegroundWindow();
+
+
+        public static IEnumerable<Process> GetChildProcesses(Process process)
+        {
+            List<Process> children = new List<Process>();
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(String.Format("Select * From Win32_Process Where ParentProcessID={0}", process.Id));
+
+            foreach (ManagementObject mo in mos.Get())
+            {
+                children.Add(Process.GetProcessById(Convert.ToInt32(mo["ProcessID"])));
+            }
+
+            return children;
+        }
+
     }
 }
